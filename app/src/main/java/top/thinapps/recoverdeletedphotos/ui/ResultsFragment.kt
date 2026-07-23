f19db55ac7e6fd535b1034eedafdf483b327ce17
Jesse Nickles <jesse@littlebizzy.com>
2026-07-24 03:01:49 +0700

    Use explicit view imports

--- app/src/main/java/top/thinapps/recoverdeletedphotos/ui/ResultsFragment.kt
+++ app/src/main/java/top/thinapps/recoverdeletedphotos/ui/ResultsFragment.kt
@@ -3,7 +3,10 @@ package top.thinapps.recoverdeletedphotos.ui
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.activity.OnBackPressedCallback
